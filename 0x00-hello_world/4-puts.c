0cd032d484d84519ede5df93e7c6bb65b7d00e6d
Olusola4 <babatopejamessunday18@gmail.com>
2023-03-21 07:36:57 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- *main - print a line of code using puts
+ * main - print a line of code using puts
  *
  *return: 0
- */
+*/
+
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+Puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
